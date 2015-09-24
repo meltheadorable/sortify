84ce9083ef5fc5ebafaf565136993c600a39495a
Melody <meltheadorable@gmail.com>
2015-09-24 13:12:44 -0400

    code style updates

--- lib/sortify.rb
+++ lib/sortify.rb
@@ -2,24 +2,19 @@ require 'whitelist_scope'
 
 module Sortify
   include WhitelistScope
-  def sort_option(name, body)
-    whitelist_scope name, body
-  end
+  alias_method :sort_option, :whitelist_scope
+  alias_method :sort_options, :whitelist
 
   def default_sort_option(name)
     @default_sort_option = name.to_sym
   end
 
-  def sort_options
-    return whitelist
-  end
-
-  def sortify(sort_option = "")
+  def sortify(sort_option = '')
     if sort_option.empty?
       begin
         call_whitelisted_scope(@default_sort_option)
       rescue
-        raise NoMethodError, "The default sort option you provided, '#{@default_sort_option.to_s}', does not exist."
+        raise NoMethodError, "The default sort option you provided, '#{@default_sort_option}', does not exist."
       end
     else
       call_whitelisted_scope(sort_option)
